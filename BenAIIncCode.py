36e833547c9948ec98c4500941670dafa4c4c9fa
Nirman Dave <nirman.dave@hotmail.com>
2014-07-16 01:48:33 +0530

    Update BenAIIncCode.py

--- BenAIIncCode.py
+++ BenAIIncCode.py
@@ -3,10 +3,10 @@
 """
 Name:                         Benedict&Pack
 Version:                      1.0
+Author:                       Nirman Dave
 Description:                  An artificial intelligence system that conducts basic functions.
                               The package allows anyone to replicate this system, using few
                               lines of code and embed into apps.
-Author:                       Nirman Dave
 Publisher:                    SourceNet
 Publisher's website:          www.sourcenet.in
 
